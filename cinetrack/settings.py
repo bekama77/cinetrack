641b53e329f280c8d0898aad42991f68b9e8d364
Aleksandar Enev <aleksenev5@gmail.com>
2026-04-08 10:31:49 +0300

    Update settings.py

--- cinetrack/settings.py
+++ cinetrack/settings.py
@@ -84,7 +84,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": "/home/site/data/db.sqlite3",
         }
     }
 
